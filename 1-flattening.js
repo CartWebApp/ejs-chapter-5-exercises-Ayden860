c7b4d147b582073c86c65cdf0d9e4771d4541d9a
Ayden860 <134655473+Ayden860@users.noreply.github.com>
2024-02-14 09:50:03 -0800

    Trial and error

--- 1-flattening.js
+++ 1-flattening.js
@@ -1,5 +1,48 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
-
+// let combination = arrays[0] + "," + arrays[1] + "," + arrays[2];
+let combination = [];
+for(let i = 0; i < arrays.length; i++)
+{
+    combination += arrays[i] + ",";
+}
+// for (let char of arrays) {
+//     char += arrays
+//     console.log(char);
+//   }
+// const all = [];
+// for(let i = 0; i < arrays.length; i++)
+// {
+    // all.push(arrays.forEach(i));
+    // all += arrays[i];
+    // if(Array.isArray(arrays))
+    // {
+        // console.log("Hello");
+        // arrays[i] + ",";
+        // all arrays[i];
+    // }
+    // return arrays;
+// }
+combination.pop(-1);
 
 // → [1, 2, 3, 4, 5, 6]
+console.log(combination);
+
+/*  Use this to study
+const flatten = (arr) => {
+    const result = [];
+    arr.forEach((item) => {
+      if (Array.isArray(item)) {
+        result.push(...flatten(item));
+      } 
+      else {
+        result.push(item);
+      }
+    });
+    return result;
+  };
+  
+  // Usage
+  const nested = [1, 2, 3, [4, 5, [6, 7], 8, 9]];
+  console.log(flatten(nested)); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  */
